c506231625c1e1d9a33f5d82d48a847a51058f96
Samuel Gustin <gustin.daniel1@gmail.com>
2021-01-03 14:13:48 +0200

    fixed err 500

--- reuser/views.py
+++ reuser/views.py
@@ -74,6 +74,6 @@ class BotView(APIView):
 
             result = q.enqueue(test.main, 4)
 
-            return Response(result, status=status.HTTP_200_OK)
+            return Response(status=status.HTTP_200_OK)
         except Exception as e:
             return Response(status=status.HTTP_400_BAD_REQUEST)
